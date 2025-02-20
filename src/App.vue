8bb55cf0e68282fe30f2505077fa920ffd723137
okskmk2 <okskmk2@naver.com>
2025-02-20 21:36:10 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
-  <div>test</div>
+  <div>
+    <select v-model="$i18n.locale">
+      <option value="en">English</option>
+      <option value="id">Bahasa Indonesia</option>
+      <option value="ko">한국어</option>
+    </select>
+    <div>{{ $t("message.hello") }}</div>
+  </div>
 </template>
-<script></script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,37 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { fetchData } from "./firestore";
-
-createApp(App).mount("#app");
+import { createI18n } from "vue-i18n";
 
 fetchData("posts").then((res) => {
   console.log(res);
 });
+
+const initLocale = navigator.language.substring(0, 2);
+
+const i18n = createI18n({
+  locale: initLocale || "en",
+  fallbackLocale: "en",
+  messages: {
+    ko: {
+      message: {
+        hello: "안녕하세요",
+      },
+    },
+    en: {
+      message: {
+        hello: "hello world",
+      },
+    },
+    id: {
+      message: {
+        hello: "Selamat siang",
+      },
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(i18n);
+app.mount("#app");
